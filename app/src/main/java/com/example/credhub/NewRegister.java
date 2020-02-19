7a2df3b70abc03d4d1d779f499b0d5b83a1895aa
carloscanibano <garciacanibanocarlos@gmail.com>
2020-02-19 11:57:51 +0100

    feat: add database register

--- app/src/main/java/com/example/credhub/NewRegister.java
+++ app/src/main/java/com/example/credhub/NewRegister.java
@@ -2,14 +2,21 @@ package com.example.credhub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -50,5 +57,31 @@ public class NewRegister extends AppCompatActivity {
                 Toast.makeText(NewRegister.this,clickedItem,Toast.LENGTH_LONG).show();
             }
         });
+
+        Button nuevo_registro_action = findViewById(R.id.nuevo_registro_action);
+
+        nuevo_registro_action.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                EditText campo_usuario = findViewById(R.id.campo_password);
+                EditText campo_password = findViewById(R.id.campo_password);
+
+                if ((campo_usuario.getText().toString().trim().length() == 0) ||
+                        (campo_password.getText().toString().length() == 0)) {
+                    Toast.makeText(NewRegister.this,"Los campos de usuario y contraseña son obligatorios",Toast.LENGTH_LONG).show();
+                } else {
+                    DatabaseHelper dbHelper = new DatabaseHelper(getApplicationContext());
+                    SQLiteDatabase db = dbHelper.getWritableDatabase();
+                    ContentValues values = new ContentValues();
+                    values.put(Database.DatabaseEntry.COLUMN_NAME_1, "");
+                    values.put(Database.DatabaseEntry.COLUMN_NAME_2, campo_usuario.getText().toString());
+                    values.put(Database.DatabaseEntry.COLUMN_NAME_3, campo_password.getText().toString());
+                    long newRowId = db.insert(Database.DatabaseEntry.TABLE_NAME, null, values);
+                    Toast.makeText(NewRegister.this,"Registro añadido con id: " + newRowId,Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
